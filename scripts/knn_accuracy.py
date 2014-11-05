3caf72f2d1d032ec69e6e92a5b8e105597d757ac
kate-mcardle <kate.mca@gmail.com>
2014-11-04 23:58:44 -0600

    Completed accuracy calculations

--- scripts/knn_accuracy.py
+++ scripts/knn_accuracy.py
@@ -2,19 +2,17 @@ import numpy as np
 import sys
 import run_sklearn
 
-# from command line: python knn_accuracy.py chen_knn/example.dat chen_knn/example.knn chen 12 560 1965
-# in interpreter: knn_accuracy.get_accuracy(['chen_knn/example.dat', 'chen_knn/example.knn', 'chen', 12, 560, 1965)
+# Usage:
+# - From command line: 
+#     python knn_accuracy.py path_to_input_data path_to_chens_output k d N
+#     example: python knn_accuracy.py ../data/frey.dat ../data/frey.knn 12 560 1965
+# - In interpreter: 
+#     knn_accuracy.compare_chen(path_to_input_data, path_to_chens_output, k, d, N)
+#     example: knn_accuracy.compare_chen('../data/frey.dat', '../data/frey.knn', 12, 560, 1965)
 
-def get_accuracy(argv):
-  input_data = argv[0]
-  knn_output = argv[1]
-  alg = argv[2]
-  k = int(argv[3])
-  d = int(argv[4])
-  N = int(argv[5])
-
+def get_accuracy(input_data, knn_output, alg, k, d, N):
   # Get ground truth
-  ground_truth = run_sklearn.get_knn_graph([input_data, k, d, N, 'brute'])
+  ground_truth = run_sklearn.get_knn_graph(input_data, k, d, N, 'brute')
   
   if alg == "chen":
     # Get knn graph file in numpy array
@@ -30,10 +28,19 @@ def get_accuracy(argv):
   elif alg == "sklearn":
     knn_graph = knn_output
 
-  if (knn_graph == ground_truth).all():
-    print "100% accuracy"
-  else:
-    print "not 100% accurate"
+  n_correct = np.where(ground_truth == knn_graph)[0].size
+  accuracy = n_correct/(N*N+0.0)
+  return accuracy
+
+def compare_chen(input_data, chen_output, k, d, N):
+  # Chen's approach:
+  chen_accuracy = get_accuracy(input_data, chen_output, 'chen', k, d, N)
+  print "Chen's approach: \taccuracy = ", chen_accuracy
+
+  # Scikit-learn's approach, using kd-tree:
+  g = run_sklearn.get_knn_graph(input_data, k, d, N, 'kd_tree')
+  sklearn_accuracy = get_accuracy(input_data, g, 'sklearn', k, d, N)
+  print "Scikit-learn's kd-tree: accuracy = ", sklearn_accuracy
 
 if __name__ == '__main__':
-  get_accuracy(sys.argv[1:])
+  compare_chen(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
--- scripts/run_sklearn.py
+++ scripts/run_sklearn.py
@@ -2,15 +2,15 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import sys
 
-# from command line: python run_sklearn.py chen_knn/example.dat 12 560 1965 kd_tree sklearn_output.txt
-# in interpreter: knn_graph = run_sklearn.get_knn_graph(['chen_knn/example.dat', 12, 560, 1965, 'kd_tree'])
+# Usage:
+# - From command line: 
+#     python run_sklearn.py path_to_input_data_file k d N algorithm
+#     example: python run_sklearn.py ../data/frey.dat 12 560 1965 kd_tree sklearn_output.txt
+# - In interpreter: 
+#     knn_graph = run_sklearn.get_knn_graph(path_to_input, k, d, N, algorithm)
+#     example: knn_graph = run_sklearn.get_knn_graph('../data/frey.dat', 12, 560, 1965, 'kd_tree')
 
-def get_knn_graph(argv):
-  data_file = argv[0]
-  k = int(argv[1])
-  d = int(argv[2])
-  N = int(argv[3])
-  alg = argv[4]
+def get_knn_graph(data_file, k, d, N, alg):
   a = np.fromfile(data_file, dtype=float).reshape((N,d))
   k_plus_1 = k+1
   nbrs = NearestNeighbors(n_neighbors=(k_plus_1), algorithm=alg).fit(a)
@@ -18,5 +18,5 @@ def get_knn_graph(argv):
   return knn_graph
 
 if __name__ == '__main__':
-  knn_graph = get_knn_graph(sys.argv[1:])
+  knn_graph = get_knn_graph(sys.argv[1:], int(argv[1]), int(argv[2]), int(argv[3]), argv[4])
   knn_graph.tofile(sys.argv[6], "\t")
